1be2dc9cabc1cd4ab039c7a18e4aab8b88686e80
csslick <digitone@naver.com>
2022-07-03 22:47:30 +0900

    index page defalut query

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
 $(function(){
-  // 페이지 업데이트(시작페이지 query 갱신)
+  /* 페이지 업데이트(시작페이지 query 갱신) - github페이지 보안으로 사용 불가
   location.reload = 'index.html?category=dog&age=1';
+  */
   
   fetch('https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.json')
     .then(function(res){
@@ -23,6 +24,9 @@ $(function(){
 
 // animal 정보 출력(category, data)
 function showAnimalList(category, data) {
+  // 시작 페이지 기본은 null이라 dog로 간주
+  if(category == null) category = 'dog';
+
   let html = '';
   // 해당 동물 category만 출력
   data.forEach(animal => {
